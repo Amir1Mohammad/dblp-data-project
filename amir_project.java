a5dc90e1cfe1c3e7b1619a24f2acd7d3e658f327
amir00100 <amir_day1374@yahoo.com>
2015-12-10 13:59:26 +0330

    yeki moonde be akhar

--- amir_project.java
+++ amir_project.java
@@ -1,6 +1,10 @@
 import java.io.*;
 import java.io.File;
+import java.lang.Integer;
+import java.lang.String;
+import java.lang.System;
 import java.util.*;
+import java.util.HashSet;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import org.xml.sax.helpers.DefaultHandler;
@@ -10,115 +14,107 @@ import org.xml.sax.SAXException;
 
 
 
-class SAXParserDemo {
+class partB {
     public static void main(String[] args){
 
+
         try {
-            System.out.println("open the xml file");
             File inputFile = new File("/home/cloner/Desktop/dblp-data.xml");
             SAXParserFactory factory = SAXParserFactory.newInstance();
             SAXParser saxParser = factory.newSAXParser();
             UserHandler userhandler = new UserHandler();
             saxParser.parse(inputFile, userhandler);
         } catch (Exception e) {
-            System.out.println("Exception1");
-            //e.printStackTrace();
+            e.printStackTrace();
         }
-    
-}
+
+
+    }
 }
 class UserHandler extends DefaultHandler {
 
 
-        //defind the name and the test :
-        boolean journal_name = false;
-        boolean people_name = false;
-        boolean Attributes = false;
-        boolean col_value = false;
-        boolean attr = false;
-        boolean attr_2 = false;
-        boolean attri = false;
-        String  ID;
-        HashSet<Integer> persons = new HashSet<>();
-        // File file = new File("test_ashrafi.txt");
-        // file.createNewFile();
-        // FileWriter writer = new FileWriter(file);     
+    boolean journal_name = false;
+    boolean people_name = false;
+    boolean Attributes = false;
+    boolean col_value_flag = false;
+    boolean attr = false;
+    boolean attr_value_flag = false;
+    boolean attri = false;
+    boolean obj_type_flag = false;
+    boolean title_flag = false;
+    String  ID;
+    String name;
+    HashSet<Integer> journals = new HashSet<>();
+    HashSet<String> titles = new HashSet<>();
 
 
     @Override
     public void startElement(String uri,String localName, String qName, Attributes attributes)throws SAXException{
-        try{
-        
 
-                if (qName.equalsIgnoreCase("ATTRIBUTES")){
-                    // System.out.println("Man Injam (ATTRIBUTES)");
-                    attr = true;
-                }if (qName.equalsIgnoreCase("ATTRIBUTE")){
-                    // System.out.println("Man Injam (ATTRIBUTE)");
-                    attri = true;
-                    String name;
-                    name = attributes.getValue("NAME");
-                }if (qName.equalsIgnoreCase("ATTR-VALUE")){
-                    // System.out.println("Man Injam (ATTR-VALUE) ");
-                    attr_2 = true;
-                    ID = attributes.getValue("ITEM-ID");
-                }if(qName.equalsIgnoreCase("COL-VALUE")){
-                    // System.out.println("Man Injam (COL-VALUE)");
-                    col_value = true;
-                    // System.out.println("END The first Algorithm .");
-                }
-            }catch(Exception g){
-                System.out.println("Exception2");
-                //g.printStackTrace();
-            }
+
+
+        if (qName.equalsIgnoreCase("ATTRIBUTES")) {
+            attr = true;
         }
-            
 
-//the second Algorithm .
-   @Override
-     public void endElement(String uri, 
-        String localName, String qName) throws SAXException {
+        if (qName.equalsIgnoreCase("ATTRIBUTE")) {
+            attri = true;
+            name = attributes.getValue("NAME");
+            if (Objects.equals(name, "object-type"))
+                obj_type_flag = true;
+            else if (Objects.equals(name, "title"))
+                title_flag = true;
+
+        }
+        if (qName.equalsIgnoreCase("ATTR-VALUE")) {
+            attr_value_flag = true;
+            ID = attributes.getValue("ITEM-ID");
+        }
         if (qName.equalsIgnoreCase("COL-VALUE")) {
-           col_value = false;
-        }else if (qName.equalsIgnoreCase("ATTRIBUTES")){
+            col_value_flag = true;
+
+        }
+
+
+    }
+
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        if (qName.equalsIgnoreCase("ATTRIBUTE")) {
+            if (obj_type_flag) obj_type_flag = false;
+            if (title_flag) title_flag = false;
+        }
+        if (qName.equalsIgnoreCase("COL-VALUE")) {
+            col_value_flag = false;
+
+        }
+        if (qName.equalsIgnoreCase("ATTR-VALUE")) {
+            attr_value_flag = false;
+        }
+
+        if (qName.equalsIgnoreCase("ATTRIBUTES")){
             attr = false;
-            // writer.close();
-            System.out.println(persons);
-            }
-     }
+            System.out.println(titles);
+        }
+    }
 
     @Override
     public void characters(char ch[],int start, int length)throws SAXException {
-        if (col_value){        
-            String str = new String(ch, start, length);
-            if (Objects.equals(str , "journal")){
-              try{
-                  persons.add(Integer.parseInt(ID));
+        String str = new String(ch, start, length);
+        if (obj_type_flag && col_value_flag){
+            if (Objects.equals(str , "journal")) {
+                journals.add(Integer.parseInt(ID));
 
-                  // writer.write(ID+"\n"); 
-                  // writer.flush(); //flush the writer
-
-                  // create a file reader Object :
-                  //FileReader fr = new FileReader(file);
-                  //char [] a = new char[50];
-                  //fr.read(a);
-                  //for(char c : a)
-                  // fr.close();
-
-
-              }catch(Exception e){
-                 System.out.println("Cant create txt file :(");
-                 e.printStackTrace();
-              }
-      
             }
-        
-        //e.printStackTrace();
+
         }
-        else if (attri){
-          String str = new String(ch, start, length);
-            if (Objects.equals(str , "title")){
+        if (title_flag && col_value_flag) {
+            if (journals.contains(Integer.parseInt(ID))) {
+                titles.add(str);
+            }
         }
+
     }
-  }
 }
